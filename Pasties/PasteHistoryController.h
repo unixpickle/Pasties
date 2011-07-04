2f5d662f727d56750d9f66b621f3782e968620c8
Alex Nichol <alexnichol@comcast.net>
2011-07-04 11:12:34 -0400

    Fixed missing files; began work on history

---
+++ Pasties/PasteHistoryController.h
@@ -0,0 +1,16 @@
+//
+//  PasteHistoryController.h
+//  Pasties
+//
+//  Created by Alex Nichol on 7/4/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface PasteHistoryController : NSObject {
+    NSString * historyFilePath;
+}
+
+@end
---
+++ Pasties/PasteHistoryController.m
@@ -0,0 +1,26 @@
+//
+//  PasteHistoryController.m
+//  Pasties
+//
+//  Created by Alex Nichol on 7/4/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "PasteHistoryController.h"
+
+
+@implementation PasteHistoryController
+
+- (id)init {
+    if ((self = [super init])) {
+        // Initialization code here.
+    }
+    return self;
+}
+
+- (void)dealloc {
+	
+    [super dealloc];
+}
+
+@end
